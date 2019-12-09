590acc32ff768826620dd2aaf9ce4ff11388d025
David Lopez <ldavid.x.lopezl@gmail.com>
2019-12-09 10:13:18 -0700

    Update test for house and not requireing role and patronus

--- spec/features/houses/search_members_by_house_spec.rb
+++ spec/features/houses/search_members_by_house_spec.rb
@@ -5,32 +5,12 @@ describe 'As a visior when i visit the root' do
     visit root_path
     click_button 'Search For Members'
 
-    expect(page).to have_css(".house_member", count: 21)
+    expect(page).to have_css(".house-member", count: 21)
 
-    expect(page).to have_content
-
-    within(first(".house_member")) do
+    within(first(".house-member")) do
       expect(page).to have_css(".name")
-      expect(page).to have_css(".role")
       expect(page).to have_css(".house")
-      expect(page).to have_css(".patronus")
+      expect(page).to have_content("Gryffindor")
     end
   end
 end
-
-# Endpoint documentation: https://www.potterapi.com/
-# We will be using the /characters  route
-# User Story:
-# As a user,
-# When I visit "/"
-# And I Select "Gryffindor" from the select field
-# (Note: Use the existing select field)
-# And I click "Search For Members"
-# Then I should be on page "/search"
-# Then I should see a total of the number of members for that house. (21 for Gryffindor)
-# Then I should see a list of 21 members
-# And for each of the members I should see:
-# - The name of the member
-# - The members role (if it exists)
-# - The house the member belongs to
-# - The Patronus of the member (if it exists)
